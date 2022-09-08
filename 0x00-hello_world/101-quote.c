9df0b72e85155b994d483a4735aa6f4b35f938b4
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-08 13:52:03 -0700

    Updated C program to print with write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: a value of 1
+ *
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+
+	return (1);
+}
