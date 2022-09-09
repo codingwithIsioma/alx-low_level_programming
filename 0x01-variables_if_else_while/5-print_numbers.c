487b783b2c189019b8ed4084bb4dcbaa108152c9
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-09 08:58:58 -0700

    Prints single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digits from 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
