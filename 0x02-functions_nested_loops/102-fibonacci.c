5ab0f7e0ef3b8c806b15f392c6925d2daea59716
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-13 13:18:10 -0700

    Quick fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-	int num1 = 1, num2 = 2, sum, i;
+	int i;
+        unsigned long num1 = 1, num2 = 2, sum;
 
-	printf("%d, %d, ", num1, num2);
+	printf("%lu, %lu, ", num1, num2);
 	for (i = 3; i < 50; ++i)
 	{
 		sum = num1 + num2;
-		printf("%d", sum);
+		printf("%lu", sum);
 		num1 = num2;
 		num2 = sum;
 
