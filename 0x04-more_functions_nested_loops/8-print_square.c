74d1515c753e2e83e469d6d6c2d087efe05b000f
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-15 11:06:08 -0700

    Quick change

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,12 +12,14 @@ void print_square(int size)
 	{
 		for (hgt = 0; hgt < size; hgt++)
 		{
-			for (wid = 0; wid < size - 1; wid++)
+			for (wid = 0; wid < size; wid++)
 			{
 				_putchar('#');
 			}
-			_putchar('#');
+			if (hgt == size - 1)
+				continue;
 			_putchar('\n');
 		}
 	}
+	_putchar('\n');
 }
