8fd29901da42a0ec6f90b7782d26472757c7468b
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-19 09:08:59 -0700

    Updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates teh value it points to 98
+ * @n: the parameter
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+C - Pointers, arrays and strings
+
+Tasks
+0. Writes a function that takes a pointer to an int as parameter
+and updates the value it points to 98.
