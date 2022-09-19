d2ecead8f53470e68d2246c979ae50452ed6967e
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-19 13:18:01 -0700

    Generates password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates password
+ * Return: Always 0
+ */
+int main(void)
+{
+	char password;
+	int i;
+
+	srand(time(0));
+	while (i <= 2645)
+	{
+		password = rand() % 128;
+		i += password;
+		putchar(password);
+	}
+	putchar(2772 - x);
+
+	return (0);
+}
