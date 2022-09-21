42ba9f887f035053d6441448f8d950af5472c866
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-21 08:50:40 -0700

    Changes lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercases letters to uppercase
+ * @s: the string to be changed
+ * Return: the changed string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+
+		i++;
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 void print_array(int *a, int n);
+char *string_toupper(char *s);
 
 #endif
