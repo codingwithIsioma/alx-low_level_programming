92575f9ea61d052e182c6fae959bd02d3177497f
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-21 07:01:05 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: the destination of the string
+ * @src: the string to be copied
+ * @n: the number of chars to be copied
+ * Return: the value of dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int length, i = 0;
+
+	/* store the len of src in the length var */
+	length = 0;
+	while (src[i++])
+		++length;
+
+	/* copy src to dest */
+	for (i = 0; src[i] && i < n; ++i)
+		dest[i] = src[i];
+
+	/* terminating the dest string */
+	for (i = length; i < n; ++i)
+		dest[length] = '\0';
+
+	return (dest);
+}
