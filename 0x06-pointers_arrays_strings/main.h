3258e2288ca6b0227d1b588b65da94ac7a8394aa
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-21 09:15:00 -0700

    Capitalizes a string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 void print_array(int *a, int n);
 char *string_toupper(char *s);
+char *cap_string(char *str);
 
 #endif
