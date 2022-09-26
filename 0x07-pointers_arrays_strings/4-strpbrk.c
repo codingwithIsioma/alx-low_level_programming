b5eae4013622f0a92d54421ab7b901270df9a688
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-26 10:59:03 -0700

    Searches a strinf for any set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ * @s: The string to be searched
+ * @accept: The set of bytes searched for
+ * Return: A pointer to the byte in @s
+ *         or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	/* loops through the @s string */
+	while (*s != '\0')
+	{
+		/* loops through the set of bytes @accept*/
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			/* checks if any of the chars match*/
+			if (*s == accept[i])
+			{
+				/* returns that char */
+				return (s);
+			}
+		}
+		/* increments the @s (checks the next char)*/
+		s++;
+	}
+	/* Leaves the loop if no chars match */
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -9,3 +9,15 @@ Task 0 - memset
 Task 1 - memcpy
 
 	Writes a function that copies memory area.
+
+Task 2 - strchr
+
+	Write a function that locates a character in a string.
+
+Task 3 - strspn
+
+	Write a function that gets the length of a prefix substring.
+
+Task 4 - strpbrk
+
+	Write a function that searches a string for any of a set of bytes.
