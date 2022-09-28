ae74d99d0fa76d3690b7df0921d4d35de2699885
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-28 07:51:05 -0700

    Prints string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: The string to be reversed.
+ */
+void _print_rev_recursion(char *s)
+{
+	/* if the string is not at the end*/
+	if (*s != '\0')
+	{
+		/*
+		 * move to the next char,
+		 * stacks the char from the first to last
+		 */
+		_print_rev_recursion(s + 1);
+	}
+	/*
+	 * when it gets to the terminating null byte
+	 * start printing from the top of the stack
+	 * to the bottom (first char of the actual string)
+	 */
+	_putchar(*s);
+}
