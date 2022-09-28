b95d141762c00f4c883b2097991453df4fb4864e
Blessing Ezekiel <celezekiel02@gmail.com>
2022-09-28 08:43:29 -0700

    Returns the length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,13 +10,14 @@ int _strlen_recursion(char *s)
 	/* checks if the string char is not a null byte*/
 	if (*s != '\0')
 	{
+		return (0);
+	}
 		/*
 		 * iterates over each char and adds 1 to it
 		 * when it gets to a null byte, it returns 0
 		 * then adds from 0 down the stack till it
 		 * gets to the actual first char (adding 1's
 		 * all the way down)
-		 */
-		return (_strlen_recursion(s + 1) + 1);
-	}
+	*/
+	return (_strlen_recursion(s + 1) + 1);
 }
