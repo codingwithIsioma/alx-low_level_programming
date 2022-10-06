a559405832ea10e81763bdb642d425eab935ac6d
Blessing Ezekiel <celezekiel02@gmail.com>
2022-10-06 06:59:19 -0700

    Concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concated;
 	/* initialize the len of the string to @n */
-	int i, concat_len = 0, len = n;
+	unsigned int i, concat_len = 0, len = n;
 
 	if (s1 == NULL)
 		s1 = "";
