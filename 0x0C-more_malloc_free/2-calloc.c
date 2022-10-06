3fbed19be51a1e5a09bce9bf3096fa703ffde03c
Blessing Ezekiel <celezekiel02@gmail.com>
2022-10-06 07:39:36 -0700

    Allocates memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array using malloc
+ *
+ * @nmemb: An array of elements
+ * @size: The size of the bytes to be allocated
+ *
+ * Return: A pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *memory;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	memory = malloc(nmemb * size);
+
+	if (memory == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		memory[i] = 0;
+
+	return (memory);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -12,4 +12,12 @@ Task 1 - string_nconcat
 
 	Write a function that concatenates two strings.
 
-	Prototype: char *string_nconcat(char *s1, char *s2, unsigned int n); 
+	Prototype: char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+Task 2 - _calloc
+
+	Write a function that allocates memory for an array, using malloc.
+
+	Prototype: void *_calloc(unsigned int nmemb, unsigned int size);
+
+Task 3 -  
