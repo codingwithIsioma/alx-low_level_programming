e86475118821dee21840652dca265c99177d8c2b
Blessing Ezekiel <celezekiel02@gmail.com>
2022-10-10 10:28:49 -0700

    Adds a new type struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Defines the name, age and owner of dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif
