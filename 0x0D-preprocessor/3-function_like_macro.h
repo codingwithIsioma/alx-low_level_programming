1a814cd13c4784bd0a29ffae5f7e2fd3e4ef0572
Blessing Ezekiel <celezekiel02@gmail.com>
2022-10-10 09:14:01 -0700

    Quick change

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-/*
- * File: 3-funxtion_like_macro.h
- * Desc: Header file
- */
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) < 0 ? (-1 * (x)) : x)
 
 #endif
