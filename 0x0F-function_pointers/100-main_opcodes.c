c7a6fd06ab0aa6c91a95d73b73102e031f866a31
Blessing Ezekiel <celezekiel02@gmail.com>
2022-10-12 15:14:17 -0700

    Writes opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints opcodes
+ * @argc: Number of arguments passed
+ * @argv: An array of pointers
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	int (*addr)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+	{
+		opcode = *(unsigned char *)addr;
+		printf("%.2x", opcode);
+
+		if (i == bytes - 1)
+			continue;
+
+		printf(" ");
+		addr++;
+	}
+	printf("\n");
+
+	return (0);
+}
